950d7c69883fe4ecc59fd5a7de2b99615d8d3bf7
ChiVien <reddragongamingvn@gmail.com>
2022-04-14 16:23:35 +0700

    Add project files.

---
+++ Tetris/Models/ScoreModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Models
+{
+    internal class ScoreModel
+    {
+        public string NickName { get; set; }
+        public int Score { get; set; }
+        public DateTime dateTime { get; set; }
+        
+        public ScoreModel()
+        {
+
+        }
+
+    }
+}
